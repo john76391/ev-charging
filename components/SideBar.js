98c71a337dac5af26705252e4c5726e62daa17db
John <john@test.com>
2024-07-21 13:50:10 +0800

    完成登出

--- components/SideBar.js
+++ components/SideBar.js
@@ -3,8 +3,50 @@ import Image from "next/image";
 import Link from "next/link";
 import { MdLogout } from "react-icons/md";
 import axiosInstance from "@/services/axios-instance";
+import Swal from "sweetalert2";
+import { useRouter } from "next/router";
+import { useAuth } from "../hooks/use-auth";
 
 export default function SideBar() {
+  const router = useRouter();
+  const { auth, setAuth } = useAuth();
+
+  const handleLogout = async () => {
+    try {
+      const res = await axiosInstance.post("/users/logout");
+
+      // 成功登出的回應
+      if (res.data.status === "success") {
+        Swal.fire({
+          title: "成功登出",
+          icon: "success",
+          didClose: () => {
+            // 成功登出後導向登入頁面
+            router.push("/member/login");
+          },
+        });
+      } else {
+        // 登出失敗的回應
+        Swal.fire({
+          title: res.data.message,
+          icon: "error",
+          didClose: () => {
+            // 登出失敗後導向登入頁面
+            router.push("/member/login");
+          },
+        });
+      }
+
+      // 登出成功與否都設定全域狀態
+      setAuth({
+        isAuthenticated: false,
+        user: null,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="w-full p-3 bg-stone-800 h-screen space-y-10 flex flex-col justify-between items-center">
       <div className="space-y-10">
@@ -29,7 +71,10 @@ export default function SideBar() {
         </ul>
       </div>
       {/* 登出按鈕 */}
-      <MdLogout className="w-7 h-7 text-white self-start cursor-pointer" />
+      <MdLogout
+        onClick={handleLogout}
+        className="w-7 h-7 text-white self-start cursor-pointer"
+      />
     </div>
   );
 }
--- pages/api/users/logout.js
+++ pages/api/users/logout.js
@@ -1,22 +1,34 @@
 import cookie from "cookie";
+import authenticate from "@/middleware/authenticate";
 
-export default function handle(req, res) {
+export default async function handle(req, res) {
   try {
     if (req.method === "POST") {
-      // 設置過去的日期來清除 Cookie
-      const expires = new Date(0);
-      // 清除 Cookie
-      res.setHeader(
-        "Set-Cookie",
-        cookie.serialize("accessToken", "", {
-          httpOnly: true,
-          // 設置過期時間為過去
-          expires: expires,
-          path: "/",
-        })
-      );
+      // 驗證登入狀態
+      const result = await authenticate(req, res);
 
-      res.status(200).json({ status: "success", message: "登出成功" });
+      if (result.status === "success") {
+        // 設置過去的日期來清除 Cookie
+        const expires = new Date(0);
+        // 清除 Cookie
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("accessToken", "", {
+            httpOnly: true,
+            // 設置過期時間為過去
+            expires: expires,
+            path: "/",
+          })
+        );
+
+        // 回應登出成功
+        res.status(200).json({ status: "success", message: "登出成功" });
+      } else {
+        // 回應登出失敗
+        res
+          .status(200)
+          .json({ status: "error", message: "登出失敗，請重新登入" });
+      }
     } else {
       res.status(405).json({ status: "error", message: "Method not allowed" });
     }
